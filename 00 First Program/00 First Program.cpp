4a6f0ecc3899ab8f2fa47780a6bafcb4048617ac
Vasudev Purohit <53868512+vasudevpurohit@users.noreply.github.com>
2021-09-16 03:32:11 -0400

    Creating a master program
    This code contains all the basic syntax for a quick glance for the following :
    -"Hello World!"
    -Initializing integers/vectors/strings
    -reading the elements of a vector using a for-loop

--- 00 First Program/00 First Program.cpp
+++ 00 First Program/00 First Program.cpp
@@ -1,9 +1,70 @@
-// First Program and pushing it to the GitHub Repository
+// this code contains all the initial tutorial codes that I can glance through quickly
+
+//----Write and running my first code - Printing "Hello World"
+
+//#include<iostream>
+//using std::cout;					//here using using namespace std is not a good idea because https://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice
+//
+//
+//int main() {
+//	cout << "Hello World!"<<"\n";
+//	return 0;
+//}
+
+//----declaring a variable and a string and displaying it
+
+//#include<iostream>
+//using std::cout;
+//using std::string;
+//
+//int main() {
+//	int a{ 1 };
+//	string b;
+//	b = "My name is Vasudev Purohit";
+//
+//	cout << "The integer is: " << a<<"\n";
+//	cout << "The string is: " << b<<"\n";
+//	return 0;
+//}
+
+//----creating a 1D and 2D vector and initialising some values in it
+
+//#include<iostream>
+//#include<vector>
+//using std::cout;
+//using std::vector;
+//
+//int main() {
+//	vector<int> v1{ 1,2,3,4 };
+//	vector<vector<int>> v2{ {1,2},{3,4},{5,6} };
+//	cout << "The 1D & 2D vector was successfully initialised!" << "\n";
+//	return 0;
+//}
+
+//----printing the values of a 1D and 2D vector
 
 #include<iostream>
-using namespace std;
-int main()
-{
-	cout << "Hello World";
+#include<vector>
+
+using std::cout;
+using std::vector;
+
+int main() {
+	vector<int> v1{ 1,2,3,4 };
+	vector<vector<int>> v2{ {5,6},{7,8},{9,10} };
+
+	//for loop to iterate over the 1D vector
+	for (int i : v1) {
+		cout << i << " ";
+	}
+	cout << "\n";
+
+	//for loop to iterate over the 2D vector
+	for (vector<int> j : v2) {
+		for (int k : j) {
+			cout << k << " ";
+		}
+		cout << "\n";
+	}
 	return 0;
 }
